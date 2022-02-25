3f7083c75e20c5d936647578362a0ad1fea7f749
Adi Satria <72695667+cimoymycat@users.noreply.github.com>
2022-02-25 11:30:28 +0700

    Javasciript Projects

---
+++ template_literals.js
@@ -0,0 +1,10 @@
+const name = "Adi Satria";
+const bod = "2001";
+
+
+const greetting =
+Hello,my name  ${name}.
+umur saya  ${2022 - bod}
+;
+
+console. log (greetting);
---
+++ variable.js
@@ -0,0 +1,4 @@
+const nama = "Adi Satria";
+const major = "Informatics";
+
+console.log(nama, major);
